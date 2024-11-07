77dafa9ad2d6511ff5bede87e2ff3ae9717dc583
wubin <18519247208@qq.com>
2024-11-07 15:50:07 +0800

    remove deepbook from unit test

--- crates/sui-sdk/src/types/object.rs
+++ crates/sui-sdk/src/types/object.rs
@@ -423,7 +423,7 @@ mod serialization {
                         && name == "BFC"
                         && type_params.is_empty()
                     {
-                        return Self::GasCoin;
+                        return Self::GasCoin(s);
                     }
                 }
 
